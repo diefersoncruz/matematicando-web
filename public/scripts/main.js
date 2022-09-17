fd9369bb464da2906f95098f88bb826833909a6a
Dieferson Cruz <dieferson.cruz@outlook.com>
2022-09-16 22:17:24 -0300

    Concluído desenvolvimento das rotinas de calculo do jogo de matematica

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,64 +1,27 @@
-var btnIniciarPararJogo = document.getElementById("btnIniciarPararJogo");
-var intervalo;
+import { iniciarJogo, pararJogo } from "./controler.js";
+import { validaResultado } from "./game.js";
 
-function iniciarJogo(segundos = 90) {
-  if (intervalo === undefined) {
-    btnIniciarPararJogo.textContent = "Parar Jogo";
-    btnIniciarPararJogo.classList.remove("btnIniciarJogo");
-    btnIniciarPararJogo.classList.add("btnPararJogo");
-    iniciarCronometro(segundos);
-    geradorGameMatematica();
-  } else {
+let jogoIniciado = false;
+
+let btnIniciarPararJogo = document.querySelector("#btnIniciarPararJogo");
+
+document.querySelector("body").addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    if (jogoIniciado) {
+      validaResultado();
+    } else {
+      jogoIniciado = true;
+      iniciarJogo();
+    }
+  }
+});
+
+btnIniciarPararJogo.addEventListener("click", function () {
+  if (jogoIniciado) {
+    jogoIniciado = false;
     pararJogo();
-  }
-}
-
-function iniciarCronometro(limiteTempo = 90) {
-  let segundos = 0;
-  let minutos = 0;
-  let contador = 0;
-
-  intervalo = window.setInterval(function () {
-    if (segundos == 60) {
-      minutos++;
-      segundos = 0;
-    }
-    if (minutos == 60) {
-      segundos = 0;
-      minutos = 0;
-    }
-
-    segundos++;
-    contador++;
-    preencherMinutosSegundos(segundos, minutos);
-    if (parseInt(contador) == parseInt(limiteTempo)) {
-      pararJogo();
-    }
-  }, 1000);
-}
-
-function pararJogo() {
-  btnIniciarPararJogo.innerText = "Iniciar Jogo";
-  btnIniciarPararJogo.classList.add("btnIniciarJogo");
-  btnIniciarPararJogo.classList.remove("btnPararJogo");
-  window.clearInterval(intervalo);
-  intervalo = undefined;
-  document.getElementById("inputResultado").setAttribute("value", "");
-  document.getElementById("segundo").innerHTML = "00";
-  document.getElementById("minuto").innerHTML = "00";
-}
-
-function preencherMinutosSegundos(segundos, minutos) {
-  // Valida se os segundos são menor que 10
-  if (segundos < 10) {
-    document.getElementById("segundo").innerHTML = "0" + segundos;
   } else {
-    document.getElementById("segundo").innerHTML = segundos;
+    jogoIniciado = true;
+    iniciarJogo();
   }
-  // Valida se os minutos são menor que 10
-  if (minutos < 10) {
-    document.getElementById("minuto").innerHTML = "0" + minutos;
-  } else {
-    document.getElementById("minuto").innerHTML = minutos;
-  }
-}
+});
